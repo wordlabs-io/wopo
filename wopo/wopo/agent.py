dc039fda2645a5e2490d20a1b0d1dd502b75f013
TK <71311548+microcoder-py@users.noreply.github.com>
2023-08-03 11:12:28 +0530

    Add files via upload

---
+++ wopo/wopo/agent.py
@@ -0,0 +1,91 @@
+from prompts import Prompts
+import json 
+
+class Agent:
+    def __init__(self, prompt: str = None, data: str = None, correct_output: str = None, text_gen = None):
+        self.prompt = prompt
+        self.original_prompt = prompt 
+        self.data = data 
+        self.correct_output = correct_output
+        self.prompt_funcs = Prompts()
+        self.last_output = None
+        self.last_feedback = None
+        self.step = 0
+        self.state = []
+        self.text_gen = text_gen
+
+    
+    def update_state(self, prompt: str = None, output: str = None, feedback: str = None, score: int = None):
+        self.prompt = prompt
+        self.last_output = output
+        self.last_feedback = feedback        
+        update_dict = {
+            self.step: {
+                "prompt": prompt,
+                "output": output,
+                "feedback": feedback,
+                "score": score
+            }
+        }
+       
+        self.state.append(update_dict)
+
+        self.step += 1
+ 
+    def generate_prompt_and_output(self, prompt):
+        receive_prompt = self.text_gen(prompt)
+
+        clean_prompt = self.prompt_funcs.clean_prompt(receive_prompt)
+        clean_prompt = self.text_gen(clean_prompt)    
+        
+        receive_output = self.prompt_funcs.execute_prompt(clean_prompt, self.data)
+        receive_output = self.text_gen(receive_output)
+
+        return clean_prompt, receive_output
+
+
+    def prompt_step(self):
+        if self.step == 0:
+            design_prompt = self.prompt_funcs.initial_prompt(self.prompt, self.data)
+            prompt, output = self.generate_prompt_and_output(design_prompt)
+            correctness_check = self.check_correctness(output)
+            feedback = correctness_check["explanation"]
+            score = correctness_check['correctness']
+            self.update_state(prompt, output, feedback, score)
+            
+        else:
+            design_prompt = self.prompt_funcs.update_prompt_with_feedback(self.prompt, self.data, self.correct_output, self.last_output, self.last_feedback)
+            prompt, output = self.generate_prompt_and_output(design_prompt)
+            correctness_check = self.check_correctness(output)
+            feedback = correctness_check["explanation"]
+            score = correctness_check['correctness']
+
+            last_step = self.log_last_step()
+            if last_step["score"] <= score:
+                self.update_state(prompt, output, feedback, score)
+
+    
+    def check_correctness(self, output):
+        correctness_prompt = self.prompt_funcs.correctness_prompt(self.correct_output, output)
+        response = self.text_gen(correctness_prompt)
+        dictified = json.loads(response)
+        return dictified
+
+    def log_last_step(self):
+        return self.state[-1][self.step - 1]
+    
+    def logs(self):
+        return self.state
+
+    def flush(self, new_prompt: str = None):
+        if new_prompt is None: 
+            self.prompt = self.original_prompt
+        else: 
+            self.prompt = new_prompt
+
+        self.state = []
+        self.last_feedback = None
+        self.last_output = None 
+        self.step = 0
+
+        return self
---
+++ wopo/wopo/prompts.py
@@ -0,0 +1,59 @@
+class Prompts:
+
+    def initial_prompt(self, prompt: str, data: str = None):
+        prompt = f"""
+        Design a prompt in the following format: "Acting as a [role], [task]". The task is being performed on the data called context. Be as clear and detailed in the steps as possible. The entire prompt must be a single paragraph
+        The task is as follows: {prompt}
+        And the context for the task is as follows: {data}
+        """
+        return prompt
+
+    def update_prompt_with_feedback(self, prompt: str, data: str, expected_output: str, received_output: str, feedback: str):
+        prompt = f"""
+        You are a prompt engineer. When the following prompt was used on the specific data called data, we receieved output received_output. However, the expected output was expected_output. Rewrite the prompt to better align the data with expected_output based on the feedback provided. The expected prompt should contain instructions in English and should not involve any code
+
+        prompt: {prompt}
+        data: {data}
+        expected_output: {expected_output}
+        received_output: {received_output}
+        feedback: {feedback}
+        """
+        return prompt
+    
+    def execute_prompt(self, prompt:str, data: str):
+        prompt = f"""
+        {prompt}
+
+        {data}
+        """
+        return prompt
+
+    def clean_prompt(self, prompt: str):
+        prompt = f"""
+        Given this prompt, only retain steps relevant to the prompt
+
+        prompt: {prompt}
+        """
+
+        return prompt
+
+    def correctness_prompt(self, expected_output: str, received_output: str):
+        prompt = f"""
+        Compare these two answers. Give them a score between 0 and 100 for similarity. The response must be a JSON string with keys correctness: score, and explanation: contains explanation for why these two are different
+
+        expected_output: {expected_output}
+
+        received_output: {received_output}
+        """
+        return prompt
+
+    def consolidation_prompt(self, prompts_list):
+        prompts = "\n\n".join(prompts_list)
+
+        prompt = f"""
+        You are a prompt engineer. Extract all unique points from all the prompts. Then combine all these prompts into one single prompt that is a single paragraph. Only return the combined prompt
+
+        {prompts}
+        """
+
+        return prompt
